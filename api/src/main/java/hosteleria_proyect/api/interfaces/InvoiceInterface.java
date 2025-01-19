2c73e3f130135c7e5bb8752abef725f501b34c8e
manuelda27999 <manuelda27999@gmail.com>
2025-01-19 11:22:05 +0100

    first commit to main with full project

--- api/src/main/java/hosteleria_proyect/api/interfaces/InvoiceInterface.java
+++
@@ -1,9 +0,0 @@
-package hosteleria_proyect.api.interfaces;
-
-import hosteleria_proyect.api.entitys.Invoice;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InvoiceInterface extends JpaRepository<Invoice, Integer> {
-}
--- api/src/main/java/hosteleria_proyect/api/interfaces/ProductInterface.java
+++
@@ -1,9 +0,0 @@
-package hosteleria_proyect.api.interfaces;
-
-import hosteleria_proyect.api.entitys.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductInterface extends JpaRepository<Product, Integer> {
-}
